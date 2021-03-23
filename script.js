a1a2cc2df172a85ecb1b37e58879ec819e9f48b2
Rigel Trujillo <rig_13@yahoo.com>
2021-03-23 17:30:14 -0600

    initial release v1.0

---
+++ script.js
@@ -0,0 +1,68 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = lowerCase.toUpperCase();
+var numeric = "0123456789";
+var specialChar = "!@#$%^&*";
+var passLength = "";
+var password = "";
+var question = [4];
+
+function askLength(){
+  passLength = window.prompt("How many characters do you need for your password 8-128"); 
+}
+
+function askAtributes(){
+  question[0] = window.confirm("Do you want lowercase characters?");
+  question[1] = window.confirm("Do you want uppercase characters?");
+  question[2] = window.confirm("Do you want numeric characters?");
+  question[3] = window.confirm("Do you want special characters?");
+
+  if ( !question[0] && !question[1] && !question[2] && !question[3]){
+    alert("You have to select at least one type of character for your password, try again....");
+    askAtributes();
+  }
+  else{
+    if( question[0]){
+      password += lowerCase;
+    }
+    if( question[1]){
+      password += upperCase;
+    }
+    if( question[2]){
+      password += numeric;
+    }
+    if( question[3]){
+      password += specialChar;
+    }
+  }
+}
+
+function generatePassword(){
+  
+  askLength();
+
+  while ( isNaN(passLength) || passLength < 8  || passLength > 128 ){
+    askLength();
+  }
+  askAtributes();
+
+  var result = "";
+
+  for ( i = 0; i < passLength; i++ ) {
+    result += password.charAt(Math.floor(Math.random() * password.length));
+}
+  return result;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
